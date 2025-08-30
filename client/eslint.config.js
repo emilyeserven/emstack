52b9e73824649ae7780e597da3100d99a0fa58db
Emily Serven <emily@servenupwebsites.net>
2025-08-29 22:48:15 -0400

    Basic storybook config added

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import jsConfig from "../lintConfigs/jsConfig.config.js";
 import tsConfig from "../lintConfigs/tsConfig.config.js";
 import reactConfig from "../lintConfigs/reactConfig.config.js";
@@ -31,4 +34,4 @@ export default tseslint.config([
       globals: globals.browser,
     },
   },
-]);
+], storybook.configs["flat/recommended"]);
