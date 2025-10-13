b4a8cc2584eeb9bb94bcad3b4888f3f158299a2b
eserven <emily.e.serven@gmail.com>
2025-10-13 17:17:47 -0400

    Using types on client side

--- packages/client/src/utils/fetchFunctions.ts
+++ packages/client/src/utils/fetchFunctions.ts
@@ -1,3 +1,5 @@
-export async function fetchTest() {
+import type { Test } from "@emstack/types/src/index.js";
+
+export async function fetchTest(): Promise<Test> {
   return await fetch("http://localhost:3001/api").then(res => res.json());
 }
