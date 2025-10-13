54b7dfed5639da16a787062e7bf28ff96df9ad4e
eserven <emily.e.serven@gmail.com>
2025-10-13 17:06:39 -0400

    Made a makeEnvOptions method

--- packages/middleware/src/services/env.ts
+++ packages/middleware/src/services/env.ts
@@ -11,3 +11,13 @@ export const envOptions = {
   schema: envSchema,
   dotenv: true,
 };
+
+export const makeEnvOptions = (dirname: string) => {
+  return {
+    schema: envSchema,
+    dotenv: {
+      path: `${dirname}/.env`,
+      debug: false,
+    },
+  };
+};
