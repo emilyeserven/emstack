96c5658f13e70996940bf654b2070139838e710e
eserven <emily.e.serven@gmail.com>
2025-10-18 11:08:08 -0400

    Added middlewareGlob

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,16 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import middlewareConfig from "@emilyeserven/eslint-config/configs/middleware.js";
 import reactBundleConfig from "@emilyeserven/eslint-config/configs/reactBundle.js";
 import stylisticConfig from "@emilyeserven/eslint-config/configs/stylistic.js";
 import tailwindConfig from "@emilyeserven/eslint-config/configs/tailwind.js";
 import clientBaselineGlobConfig from "@emilyeserven/eslint-config/globConfigs/clientBaselineGlob.js";
 import configGlobConfig from "@emilyeserven/eslint-config/globConfigs/configGlob.js";
 import jsonGlobConfig from "@emilyeserven/eslint-config/globConfigs/jsonGlob.js";
+import middlewareGlobConfig from "@emilyeserven/eslint-config/globConfigs/middlewareGlob.js";
 import tsGlobConfig from "@emilyeserven/eslint-config/globConfigs/tsGlob.js";
 import { globalIgnores } from "eslint/config";
 // import storybook from "eslint-plugin-storybook";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-
-const nonClientGlobs = [
-  "packages/middleware/src/**/*.ts",
-  "packages/**/src/**/*.test.{js,ts}",
-  "packages/client/server.js",
-];
 
 export default tseslint.config([
   globalIgnores(["**/dist/**/*"]),
@@ -26,6 +20,7 @@ export default tseslint.config([
   ...clientBaselineGlobConfig,
   ...configGlobConfig,
   ...jsonGlobConfig,
+  ...middlewareGlobConfig,
   {
     files: ["packages/client/src/**/*.{jsx,tsx}"],
     extends: [...tailwindConfig],
@@ -33,9 +28,5 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-  },
-  {
-    files: nonClientGlobs,
-    extends: [...middlewareConfig],
   },
 ]);
