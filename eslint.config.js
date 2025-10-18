e3b7f9a3b1825a1d4ff7b445c173b24f8cf46e68
eserven <emily.e.serven@gmail.com>
2025-10-18 11:03:18 -0400

    Added clientBaselineGlob and configGlob

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import importConfig from "@emilyeserven/eslint-config/configs/import.js";
 import jsConfig from "@emilyeserven/eslint-config/configs/js.js";
 import jsonConfig from "@emilyeserven/eslint-config/configs/json.js";
 import middlewareConfig from "@emilyeserven/eslint-config/configs/middleware.js";
 import reactBundleConfig from "@emilyeserven/eslint-config/configs/reactBundle.js";
 import stylisticConfig from "@emilyeserven/eslint-config/configs/stylistic.js";
 import tailwindConfig from "@emilyeserven/eslint-config/configs/tailwind.js";
+import clientBaselineGlobConfig from "@emilyeserven/eslint-config/globConfigs/clientBaselineGlob.js";
+import configGlobConfig from "@emilyeserven/eslint-config/globConfigs/configGlob.js";
 import tsGlobConfig from "@emilyeserven/eslint-config/globConfigs/tsGlob.js";
 import { globalIgnores } from "eslint/config";
 // import storybook from "eslint-plugin-storybook";
@@ -21,31 +22,13 @@ const nonClientGlobs = [
 export default tseslint.config([
   globalIgnores(["**/dist/**/*"]),
   ...stylisticConfig,
-  ...tsGlobConfig,
   ...reactBundleConfig,
-  {
-    files: ["**/*.{js,ts,jsx,tsx}"],
-    ignores: nonClientGlobs,
-    extends: [...jsConfig, ...importConfig],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: {
-        ...globals.browser,
-        __dirname: true,
-      },
-    },
-  },
+  ...tsGlobConfig,
+  ...clientBaselineGlobConfig,
+  ...configGlobConfig,
   {
     files: ["packages/client/src/**/*.{jsx,tsx}"],
     extends: [...tailwindConfig],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
-  {
-    files: ["**/*.config.{js,ts}"],
-    extends: [...jsConfig],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
