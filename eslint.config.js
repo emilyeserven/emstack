b7938b307e87c925b5df78337dbb3eef0d4eb7e4
eserven <emily.e.serven@gmail.com>
2025-10-18 11:05:01 -0400

    Added jsonGlob

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,11 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import jsConfig from "@emilyeserven/eslint-config/configs/js.js";
-import jsonConfig from "@emilyeserven/eslint-config/configs/json.js";
 import middlewareConfig from "@emilyeserven/eslint-config/configs/middleware.js";
 import reactBundleConfig from "@emilyeserven/eslint-config/configs/reactBundle.js";
 import stylisticConfig from "@emilyeserven/eslint-config/configs/stylistic.js";
 import tailwindConfig from "@emilyeserven/eslint-config/configs/tailwind.js";
 import clientBaselineGlobConfig from "@emilyeserven/eslint-config/globConfigs/clientBaselineGlob.js";
 import configGlobConfig from "@emilyeserven/eslint-config/globConfigs/configGlob.js";
+import jsonGlobConfig from "@emilyeserven/eslint-config/globConfigs/jsonGlob.js";
 import tsGlobConfig from "@emilyeserven/eslint-config/globConfigs/tsGlob.js";
 import { globalIgnores } from "eslint/config";
 // import storybook from "eslint-plugin-storybook";
@@ -26,17 +25,10 @@ export default tseslint.config([
   ...tsGlobConfig,
   ...clientBaselineGlobConfig,
   ...configGlobConfig,
+  ...jsonGlobConfig,
   {
     files: ["packages/client/src/**/*.{jsx,tsx}"],
     extends: [...tailwindConfig],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
-  {
-    files: ["**/*.json"],
-    extends: [...jsConfig, ...jsonConfig],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
