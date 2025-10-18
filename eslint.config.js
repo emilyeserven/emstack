bfeb96b0c828699e755ae5bc53d45ef87965b62d
eserven <emily.e.serven@gmail.com>
2025-10-18 11:38:23 -0400

    Using the default emstack config for linting now

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,7 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import stylisticConfig from "@emilyeserven/eslint-config/configs/stylistic.js";
-import clientGlobConfig from "@emilyeserven/eslint-config/globConfigs/clientGlob.js";
-import configGlobConfig from "@emilyeserven/eslint-config/globConfigs/configGlob.js";
-import jsonGlobConfig from "@emilyeserven/eslint-config/globConfigs/jsonGlob.js";
-import middlewareGlobConfig from "@emilyeserven/eslint-config/globConfigs/middlewareGlob.js";
-import tsGlobConfig from "@emilyeserven/eslint-config/globConfigs/tsGlob.js";
-import { globalIgnores } from "eslint/config";
-// import storybook from "eslint-plugin-storybook";
+import emstackConfig from "@emilyeserven/eslint-config";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config([
-  globalIgnores(["**/dist/**/*"]),
-  ...stylisticConfig,
-  ...tsGlobConfig,
-  ...clientGlobConfig,
-  ...configGlobConfig,
-  ...jsonGlobConfig,
-  ...middlewareGlobConfig,
+  ...emstackConfig,
 ]);
