d6d37fa11738bf45e0c24c5550ddec51f070f986
eserven <emily.e.serven@gmail.com>
2025-11-12 18:19:34 -0500

    Middleware tests work

--- packages/middleware/src/routes/api/routes.ts
+++ packages/middleware/src/routes/api/routes.ts
@@ -2,7 +2,7 @@ import { JsonSchemaToTsProvider } from "@fastify/type-provider-json-schema-to-ts
 import { FastifyInstance } from "fastify";
 
 import apiRoot from "./root.ts";
-import apiTest from "./test.ts";
+import apiTest from "./testRoute.ts";
 
 export default async function (server: FastifyInstance) {
   const fastify = server.withTypeProvider<JsonSchemaToTsProvider>();
