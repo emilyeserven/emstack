bdc2ebbdfe4b29ed5af16c23bc6082761416b5c9
eserven <emily.e.serven@gmail.com>
2025-11-21 18:34:05 -0500

    middleware, lint-staged test update

--- packages/middleware/src/tests/test.test.js
+++ packages/middleware/src/tests/test.test.js
@@ -1,6 +1,10 @@
 import { test } from "node:test";
 import assert from "assert";
 
-test("passing test", (t) => {
+test("passing test, strictEqual", (t) => {
   assert.strictEqual(1, 1);
+});
+
+test("passing test, notStrictEqual", (t) => {
+  assert.notStrictEqual(1, 2);
 });
